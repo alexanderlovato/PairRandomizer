5ec7c15a108d086533804aa4b616dc1eb0d6841b
Alexander Lovato <alex.lovato88@gmail.com>
2016-09-06 18:32:56 -0600

    Initial Commit
    Signed-off-by: Alexander Lovato <alex.lovato88@gmail.com>

---
+++ PairRandomizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PairRandomizer
+//
+//  Created by Alexander Lovato on 9/6/16.
+//  Copyright © 2016 Nonsense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
